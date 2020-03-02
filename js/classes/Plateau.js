77c98ab3b791adf45326ae85806f99c906af8a00
stephane <brule.stephane@yahoo.fr>
2020-03-02 15:16:39 +0100

    ajout des commentaire

--- js/classes/Plateau.js
+++ js/classes/Plateau.js
@@ -9,37 +9,36 @@ class Plateau {
     creer() {
         let body = document.getElementsByTagName("body")[0];
         
-        // creates a <table> element and a <tbody> element
+        // Création de l'élément <table> et d'un élément <tbody>
         let tbl = document.createElement("table");
         let tblBody = document.createElement("tbody");
         let resultat1 = 0;
         
-        // creating all cells
+        // Création de toutes les cellules du tableau
         for (let i = 0; i < 10; i++) {
-            // creates a table row
-            let row = document.createElement("tr");
+            // Création des lignes du tableau
+            let ligne = document.createElement("tr");
         
             for (let j = 0; j < 10; j++) {
-                // Create a <td> element and a text node, make the text
-                // node the contents of the <td>, and put the <td> at
-                // the end of the table row
-                var cell = document.createElement("td");
-                var resultat = cell.setAttribute("id", "" + i + j);
-                //let cellText = document.createTextNode(i + " " + j);
-                //cell.appendChild(cellText);
-                row.appendChild(cell);
+                // Création des éléments <td>
+                var cellules = document.createElement("td");
+                /* Création des id dynamiquements aux cellules en récupérant la somme de i + j et
+                *  stock le resultat dans une propriété resultat
+                */ 
+                var resultat = cellules.setAttribute("id", "" + i + j);
+                ligne.appendChild(cellules);
             }
 
         
-            // add the row to the end of the table body
-            tblBody.appendChild(row);
+            // Ajoute la ligne à la fin du tableau
+            tblBody.appendChild(ligne);
         }
         
-        // put the <tbody> in the <table>
+        // Mettre le <tbody> dans <table>
         tbl.appendChild(tblBody);
-        // appends <table> into <body>
+        // Ajouter <table> dans <body>
         body.appendChild(tbl);
-        // sets the border attribute of tbl to 2;
+        // Définitiion des attributs
         tbl.setAttribute("border", "2");
         tbl.setAttribute("width", "800");
         tbl.setAttribute("height", "500");
